0ff7307121bc1d53fa3fd6223bb218254f6e440f
duyledat197 <duyledat197>
2023-04-05 14:04:39 +0700

    draft

--- internal/domain/file.go
+++ internal/domain/file.go
@@ -1,1 +1,13 @@
 package domain
+
+import (
+	"github.com/questx-lab/backend/internal/model"
+	"github.com/questx-lab/backend/pkg/xcontext"
+)
+
+type FileDomain interface {
+	UploadImage(xcontext.Context, *model.UploadImageRequest) (*model.UploadImageResponse, error)
+}
+
+type fileDomain struct {
+}
---
+++ internal/model/file.go
@@ -0,0 +1,6 @@
+package model
+
+type UploadImageRequest struct {
+}
+
+type UploadImageResponse struct{}
