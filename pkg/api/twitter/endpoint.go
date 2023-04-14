043a9e55c5f525b69f22dcb8228b97894e41a470
anonnice1999 <124914595+anonnice1999@users.noreply.github.com>
2023-04-15 02:07:27 +0700

    feat: implement twitter reaction quest (#47)

--- pkg/api/twitter/endpoint.go
+++ pkg/api/twitter/endpoint.go
@@ -44,9 +44,13 @@ func (e *Endpoint) WithUser(id string) IEndpoint {
 	return &clone
 }
 
-func (e *Endpoint) GetUser(ctx context.Context, userID string) (User, error) {
+func (e *Endpoint) OnBehalf() string {
+	return e.UserID
+}
+
+func (e *Endpoint) GetUser(ctx context.Context, userScreenName string) (User, error) {
 	resp, err := api.New(ApiURL, "/1.1/users/show.json").
-		Query(api.Parameter{"screen_name": userID}).
+		Query(api.Parameter{"screen_name": userScreenName}).
 		GET(ctx, api.OAuth1(e.ConsumerKey, e.AccessToken, e.SigningKey))
 
 	if err != nil {
@@ -60,6 +64,33 @@ func (e *Endpoint) GetUser(ctx context.Context, userID string) (User, error) {
 	}
 
 	return user, nil
+}
+
+func (e *Endpoint) GetTweet(ctx context.Context, tweetID string) (Tweet, error) {
+	resp, err := api.New(ApiURL, "/1.1/statuses/show.json").
+		Query(api.Parameter{"id": tweetID}).
+		GET(ctx, api.OAuth1(e.ConsumerKey, e.AccessToken, e.SigningKey))
+
+	if err != nil {
+		return Tweet{}, err
+	}
+
+	id, err := resp.GetString("id_str")
+	if err != nil {
+		return Tweet{}, err
+	}
+
+	userScreenName, err := resp.GetString("user.screen_name")
+	if err != nil {
+		return Tweet{}, err
+	}
+
+	replyToTweetID, err := resp.GetString("in_reply_to_status_id_str")
+	if err != nil {
+		return Tweet{}, err
+	}
+
+	return Tweet{ID: id, UserScreenName: userScreenName, ReplyToTweetID: replyToTweetID}, nil
 }
 
 func (e *Endpoint) CheckFollowing(ctx context.Context, followingID string) (bool, error) {
@@ -78,4 +109,62 @@ func (e *Endpoint) CheckFollowing(ctx context.Context, followingID string) (bool
 	}
 
 	return resp.GetBool("relationship.source.following")
+}
+
+func (e *Endpoint) GetLikedTweet(ctx context.Context) ([]Tweet, error) {
+	resp, err := api.New(ApiURL, "/1.1/favorites/list.json").
+		Query(api.Parameter{"screen_name": e.UserID}).
+		GET(ctx, api.OAuth1(e.ConsumerKey, e.AccessToken, e.SigningKey))
+
+	if err != nil {
+		return nil, err
+	}
+
+	array, err := resp.GetArray("array")
+	if err != nil {
+		return nil, err
+	}
+
+	var tweets []Tweet
+	for _, x := range array {
+		id, err := x.GetString("id_str")
+		if err != nil {
+			return nil, err
+		}
+		tweets = append(tweets, Tweet{ID: id})
+	}
+
+	return tweets, nil
+}
+
+func (e *Endpoint) GetRetweet(ctx context.Context, tweetID string) ([]Tweet, error) {
+	resp, err := api.New(ApiURL, "/1.1/statuses/retweets/%s.json", tweetID).
+		Query(api.Parameter{"count": "100"}).
+		GET(ctx, api.OAuth1(e.ConsumerKey, e.AccessToken, e.SigningKey))
+
+	if err != nil {
+		return nil, err
+	}
+
+	array, err := resp.GetArray("array")
+	if err != nil {
+		return nil, err
+	}
+
+	var tweets []Tweet
+	for _, tw := range array {
+		id, err := tw.GetString("id_str")
+		if err != nil {
+			return nil, err
+		}
+
+		userScreenName, err := tw.GetString("user.screen_name")
+		if err != nil {
+			return nil, err
+		}
+
+		tweets = append(tweets, Tweet{ID: id, UserScreenName: userScreenName})
+	}
+
+	return tweets, nil
 }
