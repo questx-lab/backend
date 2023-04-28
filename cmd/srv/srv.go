7d45ce9b0c0e8ad172994f3af70bbc913fd6a266
duyledat197 <duyledat197>
2023-04-28 15:23:42 +0700

    test

--- cmd/srv/srv.go
+++ cmd/srv/srv.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
@@ -88,6 +89,7 @@ func (s *srv) loadLogger() {
 }
 
 func (s *srv) loadConfig() {
+	log.Println("env", getEnv("MYSQL_PASSWORD", "mysql"))
 	maxUploadSize, _ := strconv.Atoi(getEnv("MAX_UPLOAD_FILE", "2"))
 	s.configs = &config.Configs{
 		Env: getEnv("ENV", "local"),
