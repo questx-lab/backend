8e035101425b2ed9067a6098573ee004fc77727e
anonnice1999 <anonnice1999@gmail.com>
2023-05-10 17:51:57 +0000

    fix

--- pkg/dateutil/util.go
+++ pkg/dateutil/util.go
@@ -4,19 +4,6 @@ import (
 	"time"
 )
 
-// IsYesterday returns true if target is yesterday of current, otherwise, return false.
-func IsYesterday(target, current time.Time) bool {
-	lastClaimYear, lastClaimMonth, lastClaimDay := target.Date()
-	currentYear, currentMonth, currentDay := current.Date()
-	return lastClaimYear == currentYear && lastClaimMonth == currentMonth && lastClaimDay+1 == currentDay
-}
-
-// Yesterday returns the beginning of the yesterday of the current day.
-func Yesterday(current time.Time) time.Time {
-	yesterday := current.AddDate(0, 0, -1)
-	return time.Date(yesterday.Year(), yesterday.Month(), yesterday.Day(), 0, 0, 0, 0, yesterday.Location())
-}
-
 // LastWeek returns the beginning of the lastweek of the current day.
 func LastWeek(current time.Time) time.Time {
 	beginningOfCurrentWeek := current.AddDate(0, 0, int(current.Weekday()-time.Monday))
