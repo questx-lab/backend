79edc344e53965dbb997cb5ec28afa095ef0cc05
anonnice1999 <anonnice1999@gmail.com>
2023-05-11 10:27:21 +0000

    fix

--- pkg/dateutil/util.go
+++ pkg/dateutil/util.go
@@ -6,7 +6,7 @@ import (
 
 // LastWeek returns the beginning of the lastweek of the current day.
 func LastWeek(current time.Time) time.Time {
-	beginningOfCurrentWeek := current.AddDate(0, 0, int(current.Weekday()-time.Monday))
+	beginningOfCurrentWeek := current.AddDate(0, 0, -int(current.Weekday()-time.Monday))
 	lastWeek := beginningOfCurrentWeek.AddDate(0, 0, -6)
 	return time.Date(lastWeek.Year(), lastWeek.Month(), lastWeek.Day(), 0, 0, 0, 0, lastWeek.Location())
 }
