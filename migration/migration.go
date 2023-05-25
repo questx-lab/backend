3eedafa521b235319eb9f512a9d323a9c3ec17bb
anonnice1999 <anonnice1999@gmail.com>
2023-05-25 19:05:48 +0000

    fix

--- migration/migration.go
+++ migration/migration.go
@@ -9,15 +9,13 @@ import (
 	"gorm.io/gorm"
 )
 
-var Migrators = []func(context.Context) error{
-	AutoMigrate,
-}
+var Migrators = []func(context.Context) error{}
 
 func Migrate(ctx context.Context) error {
 	db := xcontext.DB(ctx)
 	var currentVersion int
 	if !db.Migrator().HasTable(&entity.Migration{}) {
-		currentVersion = 0
+		currentVersion = -1
 	} else {
 		// Find the last migration version, migrate next versions.
 		migration := entity.Migration{}
@@ -27,40 +25,38 @@ func Migrate(ctx context.Context) error {
 			}
 
 			// If not found any migration version, begin from version 1.
-			currentVersion = 1
+			currentVersion = 0
 		} else {
-			currentVersion = migration.Version + 1
+			currentVersion = migration.Version
 		}
 	}
 
+	if currentVersion == -1 {
+		if err := AutoMigrate(ctx); err != nil {
+			return err
+		}
+
+		if len(Migrators) > 0 {
+			if err := db.Create(&entity.Migration{Version: len(Migrators)}).Error; err != nil {
+				return err
+			}
+		}
+
+		xcontext.Logger(ctx).Infof("Auto migrate successfully")
+		return nil
+	}
+
 	xcontext.Logger(ctx).Infof("Begin migrating from version %d", currentVersion)
-	hasCalledAutoMigration := false
 	for version := currentVersion; version < len(Migrators); version++ {
-		// If the first version of migration (auto migration) is called, no need
-		// to call other versions.
-		if !hasCalledAutoMigration {
-			if err := Migrators[version](ctx); err != nil {
-				return err
-			}
-
-			if version == 0 {
-				hasCalledAutoMigration = true
-			}
+		if err := Migrators[version](ctx); err != nil {
+			return err
 		}
 
-		if version == 0 {
-			xcontext.Logger(ctx).Infof("Auto migration successfully")
-		} else {
-			// We still add all versions into database even if we didn't call
-			// migrator of those versions.
-			if err := db.Create(&entity.Migration{Version: version}).Error; err != nil {
-				return err
-			}
-
-			if currentVersion > 0 {
-				xcontext.Logger(ctx).Infof("Migrate version %d successfully", version)
-			}
+		if err := db.Create(&entity.Migration{Version: version}).Error; err != nil {
+			return err
 		}
+
+		xcontext.Logger(ctx).Infof("Migrate version %d successfully", version)
 	}
 	xcontext.Logger(ctx).Infof("Migration completed")
 
