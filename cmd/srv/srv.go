ede1e2186ec2c526a0979a7509b13fe236015fdd
duyledat197 <duyledat197>
2023-05-30 16:31:41 +0700

    test

--- cmd/srv/srv.go
+++ cmd/srv/srv.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -319,6 +320,7 @@ func getEnv(key, fallback string) string {
 	if !exists || value == "" {
 		value = fallback
 	}
+	log.Println(key, value)
 	return strings.Trim(value, " ")
 }
 
