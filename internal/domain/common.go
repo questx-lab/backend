8ae91d1a071726ffc2240d9743729e72558a1af6
anonnice1999 <124914595+anonnice1999@users.noreply.github.com>
2023-06-06 11:19:13 +0700

    feat: support unapprove a claimed-quest (#162)

--- internal/domain/common.go
+++ internal/domain/common.go
@@ -3,7 +3,6 @@ package domain
 import (
 	"context"
 	"errors"
-	"fmt"
 	"regexp"
 	"strings"
 	"time"
@@ -13,7 +12,6 @@ import (
 	"github.com/questx-lab/backend/internal/domain/questclaim"
 	"github.com/questx-lab/backend/internal/entity"
 	"github.com/questx-lab/backend/internal/model"
-	"github.com/questx-lab/backend/pkg/dateutil"
 )
 
 const defaultTimeLayout = time.RFC3339Nano
@@ -223,39 +221,6 @@ func processValidationData(
 
 	quest.ValidationData = structs.Map(processor)
 	return nil
-}
-
-func stringToPeriodWithTime(periodString string, current time.Time) (entity.LeaderBoardPeriodType, error) {
-	switch periodString {
-	case "week":
-		return entity.NewLeaderBoardPeriodWeek(current), nil
-	case "month":
-		return entity.NewLeaderBoardPeriodMonth(current), nil
-	}
-
-	return nil, fmt.Errorf("invalid period, expected week or month, but got %s", periodString)
-}
-
-func stringToPeriod(periodString string) (entity.LeaderBoardPeriodType, error) {
-	switch periodString {
-	case "week":
-		return entity.NewLeaderBoardPeriodWeek(time.Now()), nil
-	case "month":
-		return entity.NewLeaderBoardPeriodMonth(time.Now()), nil
-	}
-
-	return nil, fmt.Errorf("invalid period, expected week or month, but got %s", periodString)
-}
-
-func stringToLastPeriod(periodString string) (entity.LeaderBoardPeriodType, error) {
-	switch periodString {
-	case "week":
-		return entity.NewLeaderBoardPeriodWeek(dateutil.LastWeek(time.Now())), nil
-	case "month":
-		return entity.NewLeaderBoardPeriodMonth(dateutil.LastMonth(time.Now())), nil
-	}
-
-	return nil, fmt.Errorf("invalid period, expected week or month, but got %s", periodString)
 }
 
 func checkCommunityHandle(handle string) error {
