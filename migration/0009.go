67e6133d156759e2ecc9d61c8a85732ead54c33b
duyledat197 <duyledat197>
2023-06-07 22:42:40 +0700

    fix migration

--- migration/0009.go
+++ migration/0009.go
@@ -6,7 +6,14 @@ import (
 	"github.com/questx-lab/backend/pkg/xcontext"
 )
 
+// This struct is cloned from the first version of enity.PayReward. It is used
+// to keep the original table structure. If we call CreateTable with
+// entity.PayReward instead of this struct, it always creates a table with the
+// latest version, and modifying columns of this table in the future may be
+// failed.
 type BlockChainTransaction struct {
+	// NOTE: Please make sure this is the latest version Base at the time this
+	// file is created.
 	Chain       string
 	TxHash      string
 	BlockHeight int64
