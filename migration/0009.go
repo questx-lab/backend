4baac91be8f908eaa51509d7a177d3b1123f1141
duyledat197 <duyledat197>
2023-06-07 22:33:19 +0700

    fix migration

--- migration/0009.go
+++ migration/0009.go
@@ -3,12 +3,18 @@ package migration
 import (
 	"context"
 
-	"github.com/questx-lab/backend/internal/entity"
 	"github.com/questx-lab/backend/pkg/xcontext"
 )
 
+type BlockChainTransaction struct {
+	Chain       string
+	TxHash      string
+	BlockHeight int64
+	TxBytes     []byte
+}
+
 func migrate0009(ctx context.Context) error {
-	if err := xcontext.DB(ctx).Migrator().CreateTable(&entity.BlockChainTransaction{}); err != nil {
+	if err := xcontext.DB(ctx).Migrator().CreateTable(&BlockChainTransaction{}); err != nil {
 		return err
 	}
 
