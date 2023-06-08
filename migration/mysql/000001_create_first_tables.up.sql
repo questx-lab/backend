9e00a9588ad3a5a6d95daa08152bd2484402ce62
anonnice1999 <anonnice1999@gmail.com>
2023-06-08 08:23:17 +0000

    fix

--- migration/mysql/000001_create_first_tables.up.sql
+++ migration/mysql/000001_create_first_tables.up.sql
@@ -13,14 +13,6 @@ CREATE TABLE IF NOT EXISTS `users`(
     INDEX `idx_users_deleted_at`(`deleted_at`)
 );
 
-CREATE TABLE IF NOT EXISTS `oauth2` (
-    `user_id` text, 
-    `service` text, 
-    `service_user_id` text UNIQUE, 
-    PRIMARY KEY (`user_id`, `service`), 
-    CONSTRAINT `fk_oauth2_user` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
-)
-
 CREATE TABLE IF NOT EXISTS `communities` (
     `id` varchar(256), 
     `created_at` datetime NULL, 
