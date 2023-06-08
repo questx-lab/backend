6ed0112636d11ed93b8b2ef1cc96c84806a90cae
anonnice1999 <anonnice1999@gmail.com>
2023-06-08 08:25:37 +0000

    fix

--- migration/mysql/000002_add_oauth2_table.up.sql
+++ migration/mysql/000002_add_oauth2_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `oauth2` (
-    `user_id` text, 
-    `service` text, 
-    `service_user_id` text UNIQUE, 
+    `user_id` varchar(256), 
+    `service` varchar(256), 
+    `service_user_id` varchar(256) UNIQUE, 
     PRIMARY KEY (`user_id`, `service`), 
     CONSTRAINT `fk_oauth2_user` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
 );
