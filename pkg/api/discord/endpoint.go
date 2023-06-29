fd5a2d518ffeae4eddbc2a5bc2dbe3d81431ec30
anonnice1999 <124914595+anonnice1999@users.noreply.github.com>
2023-06-29 19:38:40 +0700

    fix: admin of server also connect server to community (#265)

--- pkg/api/discord/endpoint.go
+++ pkg/api/discord/endpoint.go
@@ -81,25 +81,38 @@ func (e *Endpoint) HasAddedBot(ctx context.Context, guildID string) (bool, error
 	return true, nil
 }
 
-func (e *Endpoint) CheckMember(ctx context.Context, guildID, userID string) (bool, error) {
+func (e *Endpoint) GetMember(ctx context.Context, guildID, userID string) (Member, error) {
 	resp, err := e.apiGenerator.New(apiURL, "/guilds/%s/members/%s", guildID, userID).
 		Header("User-Agent", userAgent).
 		GET(ctx, api.OAuth2("Bot", e.BotToken))
 	if err != nil {
-		return false, err
+		return Member{}, err
 	}
 
 	body, ok := resp.Body.(api.JSON)
 	if !ok {
-		return false, errors.New("invalid response")
+		return Member{}, errors.New("invalid response")
 	}
 
 	// If response has the field of code, an error is returned.
 	if _, err := body.GetInt("code"); err == nil {
-		return false, nil
+		return Member{}, nil
 	}
 
-	return true, nil
+	member := Member{User: User{ID: userID}}
+	rolesObj, err := body.Get("roles")
+	if err == nil {
+		roles, ok := rolesObj.([]any)
+		if ok {
+			for _, role := range roles {
+				if r, ok := role.(string); ok {
+					member.RoleIDs = append(member.RoleIDs, r)
+				}
+			}
+		}
+	}
+
+	return member, nil
 }
 
 func (e *Endpoint) CheckCode(ctx context.Context, guildID, code string) error {
@@ -272,7 +285,18 @@ func (e *Endpoint) GetRoles(ctx context.Context, guildID string) ([]Role, error)
 			botID = ""
 		}
 
-		roles = append(roles, Role{ID: id, Name: name, Position: position, BotID: botID})
+		permissions, err := role.GetInt("permissions")
+		if err != nil {
+			permissions = 0
+		}
+
+		roles = append(roles, Role{
+			ID:          id,
+			Name:        name,
+			Position:    position,
+			BotID:       botID,
+			Permissions: permissions,
+		})
 	}
 
 	return roles, nil
