be53f11edd765122f8d7094927f9478f0b29b1eb
anonnice1999 <anonnice1999@gmail.com>
2023-07-05 18:08:40 +0000

    fix

--- migration/mysql/000016_add_quest_index.up.sql
+++ migration/mysql/000016_add_quest_index.up.sql
@@ -1,4 +1,1 @@
-ALTER TABLE `quests` DROP IF EXISTS `index`;
-ALTER TABLE `quests` ADD COLUMN `position` INT AUTO_INCREMENT UNIQUE FIRST;
-ALTER TABLE `quests` CHANGE `position` `position` INT NOT NULL;
-ALTER TABLE `quests` DROP INDEX `position`;
+ALTER TABLE `quests` CHANGE `index` `position` INT NOT NULL;
