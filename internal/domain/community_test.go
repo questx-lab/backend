5df16e7cffc8c532f7cd8e300c1559f8f1d25bbc
anonnice1999 <anonnice1999@gmail.com>
2023-07-05 12:43:17 +0000

    fix

--- internal/domain/community_test.go
+++ internal/domain/community_test.go
@@ -4,43 +4,13 @@ import (
 	"context"
 	"testing"
 
-	"github.com/questx-lab/backend/internal/entity"
 	"github.com/questx-lab/backend/internal/model"
 	"github.com/questx-lab/backend/internal/repository"
 	"github.com/questx-lab/backend/pkg/errorx"
 	"github.com/questx-lab/backend/pkg/reflectutil"
 	"github.com/questx-lab/backend/pkg/testutil"
-	"github.com/questx-lab/backend/pkg/xcontext"
 	"github.com/stretchr/testify/require"
 )
-
-func Test_communityDomain_Create(t *testing.T) {
-	ctx := testutil.MockContextWithUserID(testutil.User1.ID)
-	testutil.CreateFixtureDb(ctx)
-	communityRepo := repository.NewCommunityRepository(&testutil.MockSearchCaller{})
-	collaboratorRepo := repository.NewCollaboratorRepository()
-	userRepo := repository.NewUserRepository()
-	questRepo := repository.NewQuestRepository(&testutil.MockSearchCaller{})
-	oauth2Repo := repository.NewOAuth2Repository()
-	gameRepo := repository.NewGameRepository()
-	domain := NewCommunityDomain(communityRepo, collaboratorRepo, userRepo,
-		questRepo, oauth2Repo, gameRepo, nil, nil, &testutil.MockPublisher{}, nil)
-
-	req := &model.CreateCommunityRequest{
-		Handle:      "test",
-		DisplayName: "for testing",
-		Twitter:     "https://twitter.com/hashtag/Breaking2",
-	}
-	resp, err := domain.Create(ctx, req)
-	require.NoError(t, err)
-
-	var result entity.Community
-	tx := xcontext.DB(ctx).Model(&entity.Community{}).Take(&result, "handle", resp.Handle)
-	require.NoError(t, tx.Error)
-	require.Equal(t, result.Handle, req.Handle)
-	require.Equal(t, result.Twitter, req.Twitter)
-	require.Equal(t, result.CreatedBy, testutil.User1.ID)
-}
 
 func Test_communityDomain_TransferCommunity(t *testing.T) {
 	ctx := testutil.MockContextWithUserID(testutil.User1.ID)
