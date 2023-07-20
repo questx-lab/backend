394c9eb83ede2e9d009db5e14a47d53df690cd55
duyledat197 <duyledat197>
2023-07-20 21:33:39 +0700

    log address

--- cmd/srv/notification_engine.go
+++ cmd/srv/notification_engine.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/ethereum/go-ethereum/rpc"
@@ -25,6 +26,7 @@ func (s *srv) startNotificationEngine(*cli.Context) error {
 	go func() {
 		xcontext.Logger(s.ctx).Infof("Start rpc notification engine on port: %s",
 			cfg.Notification.EngineRPCServer.Port)
+		log.Println("Engine RPC address: ", cfg.Notification.EngineRPCServer.Address())
 		httpSrv := &http.Server{
 			Handler: rpcHandler,
 			Addr:    cfg.Notification.EngineRPCServer.Address(),
@@ -37,6 +39,7 @@ func (s *srv) startNotificationEngine(*cli.Context) error {
 	defaultRouter := router.New(s.ctx)
 	router.Websocket(defaultRouter, "/proxy", engineServer.ServeProxy)
 
+	log.Println("Engine WS address: ", cfg.Notification.EngineWSServer.Address())
 	httpSrv := &http.Server{
 		Addr:    cfg.Notification.EngineWSServer.Address(),
 		Handler: defaultRouter.Handler(cfg.Notification.EngineWSServer),
