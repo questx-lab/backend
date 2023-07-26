d28db51988dd6177c5338051ef3f2cc6e2ef6c56
duyledat197 <duyledat197>
2023-07-26 23:23:32 +0700

    fix role

--- migration/mysql/000024_add_priority_for_role.up.sql
+++ migration/mysql/000024_add_priority_for_role.up.sql
@@ -2,7 +2,7 @@ ALTER TABLE `roles`
   ADD COLUMN IF NOT EXISTS `priority` smallint;
 
 ALTER TABLE `roles`
-  ADD COLUMN IF NOT EXISTS `color` smallint;
+  ADD COLUMN IF NOT EXISTS `color` varchar(256);
 
 ALTER TABLE `roles`
   ADD CONSTRAINT `unique_roles_community_id_priority` UNIQUE (`community_id`, `priority`);
