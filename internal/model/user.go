6d6b3e647b0d359e953a45ea5ee9e466056f6b6b
anonnice1999 <124914595+anonnice1999@users.noreply.github.com>
2023-08-03 12:42:09 +0700

    feat: add get total users for super admin (#345)

--- internal/model/user.go
+++ internal/model/user.go
@@ -74,3 +74,9 @@ type UploadAvatarRequest struct {
 }
 
 type UploadAvatarResponse struct{}
+
+type CountTotalUsersRequest struct{}
+
+type CountTotalUsersResponse struct {
+	Total int `json:"total"`
+}
