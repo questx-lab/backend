c1991f222c52ac92e82085b766104118cb1a363a
anonnice1999 <anonnice1999@gmail.com>
2023-08-09 17:56:27 +0000

    add order

--- internal/repository/follower.go
+++ internal/repository/follower.go
@@ -311,6 +311,7 @@ func (r *followerRepository) GetStreaks(
 	err := xcontext.DB(ctx).
 		Where("user_id=? AND community_id=?", userID, communityID).
 		Where("start_time>=? AND start_time <?", begin, end).
+		Order("start_time ASC").
 		Find(&streaks).Error
 	if err != nil {
 		return nil, err
