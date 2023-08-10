c8a0bf7b37a3f407c9fa2c6b26bd79e3166b6314
anonnice1999 <124914595+anonnice1999@users.noreply.github.com>
2023-08-11 00:37:50 +0700

    feat: restore check reply of twitter (#356)

--- pkg/api/twitter/interface.go
+++ pkg/api/twitter/interface.go
@@ -5,4 +5,5 @@ import "context"
 type IEndpoint interface {
 	GetUser(ctx context.Context, userScreenName string) (User, error)
 	GetTweet(ctx context.Context, author, tweetID string) (Tweet, error)
+	CheckAndGetReply(ctx context.Context, author, tweetID, replyTo string) (Tweet, error)
 }
